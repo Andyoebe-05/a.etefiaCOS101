25704d3a94404e67a42d8cd38e751bcdebb889ef
a.etefiaCOS101 <andikan-phoebe.etefia@pau.edu.ng>
2026-09-10 13:30:53 +0100

    Added Week 3

---
+++ week-3/practice_10/target/debug/practice_10.d
@@ -0,0 +1,1 @@
+C:\Users\andik\Documents\a.etefiaCOS101\week-3\practice_10\target\debug\practice_10.exe: C:\Users\andik\Documents\a.etefiaCOS101\week-3\practice_10\src\main.rs
---
+++ week-3/practice_9/target/debug/practice_9.d
@@ -0,0 +1,1 @@
+C:\Users\andik\Documents\a.etefiaCOS101\week-3\practice_9\target\debug\practice_9.exe: C:\Users\andik\Documents\a.etefiaCOS101\week-3\practice_9\src\main.rs
